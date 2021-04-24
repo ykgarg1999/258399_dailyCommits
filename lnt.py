9cd48e49b6d3fbfbcf60f16753f589de2ba74802
Yeshu Garg <ykgarg1999@gmail.com>
2021-04-25 01:42:46 +0530

    daily commit

--- lnt.py
+++ lnt.py
@@ -1,3 +1,25 @@
+print("258399 - Yeshu Garg");
+
 print('hello lnt')
 
 print(2 + 3 / 5)
+
+num = int(input())
+if(num>10):
+    print("num greater tham 10")
+else:
+    print("num smaller than 10")
+
+n = int(input())
+if(n>num):
+    print("we can say that n is greater than num")
+
+else:
+    print("the value of n is "+str(n))
+
+
+
+if(True and True):
+    print("both statements are true")
+if(n>12):
+    print("value of n is greater than 12")
